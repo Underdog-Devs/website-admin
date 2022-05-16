d54379e2a32d439f30feb0db8123555ebaa9e442
unknown <dandavisjs@protonmail.com>
2022-05-15 22:16:28 -0700

    Created a fetch many posts endpoint

---
+++ pages/api/blog/post.ts
@@ -0,0 +1,22 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import prisma from '../../../lib/prisma';
+
+export default async function (req: NextApiRequest, res: NextApiResponse) {
+	const { id } = req.body;
+
+	try {
+		const post = await prisma.blog.findUnique({
+			where: {
+				id,
+			},
+		});
+		if (post) {
+			res.status(201).json({ post });
+		}
+	} catch (e) {
+		res.status(500);
+		res.json({ error: 'Internal Server Error Creating User' });
+	} finally {
+		await prisma.$disconnect();
+	}
+}
---
+++ pages/api/blog/posts.ts
@@ -0,0 +1,16 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import prisma from '../../../lib/prisma';
+
+export default async function (req: NextApiRequest, res: NextApiResponse) {
+	try {
+		const posts = await prisma.blog.findMany();
+		if (posts) {
+			res.status(201).json({ posts });
+		}
+	} catch (e) {
+		res.status(500);
+		res.json({ error: 'Internal Server Error Creating User' });
+	} finally {
+		await prisma.$disconnect();
+	}
+}
