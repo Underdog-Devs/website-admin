a4db773104429cb4323d772dccd42c347d78daf6
Jess Bonanno <jbonanno@slack-corp.com>
2022-06-04 18:34:06 -0400

    created request password reset component and page

---
+++ pages/auth/request-password-reset/ReqeustPasswordReset.tsx
@@ -0,0 +1,54 @@
+import Link from 'next/link';
+import React, { useState } from 'react';
+import styles from '../../../components/auth/login/login.module.scss';
+
+type Props = {}
+
+export function RequestPasswordReset(props: Props) {
+	const { } = props;
+	const [email, setEmail] = useState<string>('');
+
+	const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		const { value } = e.target;
+		setEmail(value);
+	};
+
+	const handleSubmit = (e: React.SyntheticEvent) => {
+		e.preventDefault();
+		console.log(email);
+		setEmail('');
+	};
+
+	return (
+		<div className={styles.container}>
+			<img
+				className={styles.logoImage}
+				src="/images/underdogdevs-01.png"
+				alt="Underdog Devs Logo"
+				height={300}
+				width={300}
+				loading="lazy"
+			/>
+			<div className={styles.formContainer}>
+				<h4 className={styles.title}>Password Reset</h4>
+				<form className={styles.form} onSubmit={handleSubmit}>
+					<div className={styles.formField}>
+						<input
+							className={styles.formInput}
+							type="email"
+							name="email"
+							id="email"
+							value={email}
+							onChange={handleChange}
+						/>
+						<label className={styles.formLabel} htmlFor="email">Email</label>
+					</div>
+					<div className={styles.formActions}>
+						<button type="submit" className={styles.formButton}>Submit</button>
+						<Link href="/"><a className={styles.passwordResetLink}>Login</a></Link>
+					</div>
+				</form>
+			</div>
+		</div>
+	);
+}
