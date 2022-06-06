08e15973c9e4569b7ac715710a70d35341d0d973
Al <dandavisjs@protonmail.com>
2022-06-06 16:56:32 -0700

    Created 'Authors posts' endpoint

---
+++ pages/api/posts/author.ts
@@ -0,0 +1,22 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import prisma from '../../../lib/prisma';
+
+export default async function (req: NextApiRequest, res: NextApiResponse) {
+	const { authorId } = req.body;
+
+	try {
+		const post = await prisma.blog.findMany({
+			where: {
+				authorId,
+			},
+		});
+		if (post) {
+			res.status(201).json({ post });
+		}
+	} catch (e) {
+		res.status(500);
+		res.json({ error: 'Internal Server Error Requesting Author\'s Posts' });
+	} finally {
+		await prisma.$disconnect();
+	}
+}
