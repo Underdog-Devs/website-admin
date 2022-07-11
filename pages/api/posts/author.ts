72f1bfb937f40e88b0ad4b9a012351ccefecc41e
Al <dandavisjs@protonmail.com>
2022-07-10 20:27:57 -0700

    Delete a post endpoint

---
+++ pages/api/posts/author.ts
@@ -0,0 +1,22 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import prisma from '../../../lib/prisma';
+
+export default async function (req: NextApiRequest, res: NextApiResponse) {
+	const { authorId } = req.body;
+
+	try {
+		const posts = await prisma.blog.findMany({
+			where: {
+				authorId,
+			},
+		});
+		if (posts) {
+			res.status(201).json({ posts });
+		}
+	} catch (e) {
+		res.status(500);
+		res.json({ error: 'Internal Server Error Requesting Author\'s Posts' });
+	} finally {
+		await prisma.$disconnect();
+	}
+}
