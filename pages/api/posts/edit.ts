8ed7b2530439c4bdf9b9b7662a0e346a23d88284
Al <dandavisjs@protonmail.com>
2022-07-10 19:25:34 -0700

    Edit post endpoint

---
+++ pages/api/posts/edit.ts
@@ -0,0 +1,25 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import prisma from '../../../lib/prisma';
+
+export default async function (req: NextApiRequest, res: NextApiResponse) {
+	const { id, entry } = req.body;
+
+	try {
+		const post = await prisma.blog.update({
+			where: {
+				id,
+			},
+			data: {
+				entry,
+			},
+		});
+		if (post) {
+			res.status(201).json({ post, message: 'Post Was Edited Successfully' });
+		}
+	} catch (e) {
+		res.status(500);
+		res.json({ error: 'Internal Server Error Editing A Post' });
+	} finally {
+		await prisma.$disconnect();
+	}
+}
