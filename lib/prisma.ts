a71c5f148d4f82a1e080817573bcd6f1e1599bf8
Jess Bonanno <jbonanno@slack-corp.com>
2022-07-16 13:41:38 -0400

    updated prisma def

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,19 +1,35 @@
 import { PrismaClient } from '@prisma/client';
 
-let prisma: PrismaClient;
 
-if (process.env.NODE_ENV === 'production') {
-	prisma = new PrismaClient();
-} else {
-	/* global globalThis */
-	// eslint-disable-next-line no-undef
-	const globalWithPrisma = global as typeof globalThis & {
-    prisma: PrismaClient;
-  };
-	if (!globalWithPrisma.prisma) {
-		globalWithPrisma.prisma = new PrismaClient();
-	}
-	prisma = globalWithPrisma.prisma;
+declare global {
+  // allow global `var` declarations
+  // eslint-disable-next-line no-var
+  var prisma: PrismaClient | undefined
 }
+
+export const prisma =
+  global.prisma ||
+  new PrismaClient({
+    log: ['query'],
+  })
+
+if (process.env.NODE_ENV !== 'production') global.prisma = prisma
+
+
+// let prisma: PrismaClient;
+
+// if (process.env.NODE_ENV === 'production') {
+// 	prisma = new PrismaClient();
+// } else {
+// 	/* global globalThis */
+// 	// eslint-disable-next-line no-undef
+// 	const globalWithPrisma = global as typeof globalThis & {
+//     prisma: PrismaClient;
+//   };
+// 	if (!globalWithPrisma.prisma) {
+// 		globalWithPrisma.prisma = new PrismaClient();
+// 	}
+// 	prisma = globalWithPrisma.prisma;
+// }
 
 export default prisma;
