8f177f77acf6e001da033887af46ffffae86fa96
thatoldplatitude <itsfirstnamedotlastname@tutanota.com>
2022-07-21 02:15:57 -0400

    Value checks before calling out to db

--- middleware/hasUser.js
+++ middleware/hasUser.js
@@ -11,6 +11,9 @@ const hasUser = (handler) =>{
 		try {
 			console.log("Hey look at meee! I'm in a middleware WOOOOOOAAHHHH!!!");
 			let userEmail = req.body.user.email;
+			if(userEmail == null || userEmail == undefined) {
+				throw "User not found.";
+			}
 			const isUser = await prisma.user.findUnique({
 				where: {
 					email: userEmail
@@ -19,6 +22,17 @@ const hasUser = (handler) =>{
 			if(isUser == null || isUser == undefined) {
 				throw "User not found.";
 			}
+			/**
+			 * Here, once we know user exists; do we want to
+			 * make the user db obj result part of the req instead on whats being sent from the FE?
+			 * 
+			 * e.g. Operations needing a valid user will need to pass in user:{email:<String>}
+			 * this should be checked by our wicked awesome 'hasUser middleware' if the user exists,
+			 * we can then assign the user obj to the req.
+			 * req.body.user = resultFromQueryingUserTable;
+			 *  So diffrent endpoints can use email or id properties
+			 * of a user.
+			 */
 			return handler(req,res);
 		} catch (err) {
 			let msg = err.message;
