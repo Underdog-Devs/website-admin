71f718644f75b48e012dd1df6c7832f406c4cf8e
Jess Bonanno <jbonanno@slack-corp.com>
2022-09-03 19:49:12 -0400

    remove commented out code add redirect

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import { GetServerSideProps } from 'next';
+import { getSession } from 'next-auth/react';
 import styles from './dashboard.module.scss';
 import Nav from '../components/dashboard/nav';
 import Posts from '../components/blog/posts';
 import prisma from '../lib/prisma';
-import { getSession } from 'next-auth/react';
 
 type Props = {
 	posts: any;
@@ -12,19 +11,6 @@ type Props = {
 
 function Dashboard(props: Props) {
 	const { posts } = props;
-	console.log(posts);
-	// const posts = [{ title: 'Blog Title',
-	// 	text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin auctor enim nec purus fermentum sodales. Donec sed varius arcu, volutpat pretium metus.',
-	// 	author: 'username',
-	// 	date: '07/12/2022' },
-	// { title: 'Blog Title2',
-	// 	text: 'Lorem2 ipsum dolor sit amet, consectetur adipiscing elit. Proin auctor enim nec purus fermentum sodales. Donec sed varius arcu, volutpat pretium metus.',
-	// 	author: 'username2',
-	// 	date: '07/13/2022' },
-	// { title: 'Blog Title3',
-	// 	text: 'Lorem3 ipsum dolor sit amet, consectetur adipiscing elit. Proin auctor enim nec purus fermentum sodales. Donec sed varius arcu, volutpat pretium metus.',
-	// 	author: 'username3',
-	// 	date: '07/14/2022' }];
 	return (
 		<div className={styles.container}>
 			<div className={styles.main}>
@@ -39,34 +25,33 @@ function Dashboard(props: Props) {
 }
 
 export async function getServerSideProps(context: { req: any; }) {
-  const session = await getSession({ req: context.req });
-  // Redirect if user isn't logged in
-  if (!session) {
-    return {
-      redirect: {
-        destination: '/',
-        permanent: false,
-      },
-    };
-  } else {
-    // Fetch all posted jobs and include related items from Company table
-    const posts = await prisma.blog.findMany({
-      take: 3,
-      orderBy: [
-        {
-          date: 'desc',
-        },
-      ],
-    });
-    return {
-      props: {
-        posts: posts.map((post) => ({
-          ...post,
-          date: post.date.toISOString(),
-        })),
-      },
-    };
-  }
-};
+	const session = await getSession({ req: context.req });
+	// Redirect if user isn't logged in
+	if (!session) {
+		return {
+			redirect: {
+				destination: '/',
+				permanent: false,
+			},
+		};
+	}
+	// Fetch all posted jobs and include related items from Company table
+	const posts = await prisma.blog.findMany({
+		take: 3,
+		orderBy: [
+			{
+				date: 'desc',
+			},
+		],
+	});
+	return {
+		props: {
+			posts: posts.map((post) => ({
+				...post,
+				date: post.date.toISOString(),
+			})),
+		},
+	};
+}
 
 export default Dashboard;
