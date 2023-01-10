069c7e442cdc46cf419be3ed85ff1a5247bbd3cd
Al <dandavisjs@protonmail.com>
2023-01-10 12:26:36 -0800

    add env variables to next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+	reactStrictMode: true,
+	env: {
+		NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+		DATABASE_URL: process.env.DATABASE_URL,
+		NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+		SENDGRID_API_KEY: process.env.SENDGRID_API_KEY,
+	},
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
