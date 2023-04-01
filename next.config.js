9c3eb0087eca94c16059fb8a0ea0211c1b5d9b5f
alur2191 <dandavisjs@protonmail.com>
2023-04-01 16:00:07 -0700

    Changed next.config.js image optimization settings

--- next.config.js
+++ next.config.js
@@ -1,23 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
-	env: {
-		NEXTAUTH_URL: process.env.NEXTAUTH_URL,
-		DATABASE_URL: process.env.DATABASE_URL,
-		NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
-		SENDGRID_API_KEY: process.env.SENDGRID_API_KEY,
-		S3_URL: process.env.S3_URL,
-		S3_ACCESS_KEY_ID: process.env.S3_ACCESS_KEY_ID,
-		S3_SECRET_ACCESS_KEY: process.env.S3_SECRET_ACCESS_KEY,
-	},
 	images: {
 		domains: ['ud-media.s3.us-east-2.amazonaws.com'],
 		formats: ['image/avif', 'image/webp'],
-	},
-	experimental: {
-		images: {
-			allowFutureImage: true,
-		},
+		unoptimized: true,
 	},
 };
 
