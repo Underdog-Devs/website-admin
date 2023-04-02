3227bbbef58396fe5ec321f1578bfdc5d3ed2940
alur2191 <dandavisjs@protonmail.com>
2023-04-01 17:31:51 -0700

    Changed CORS settings

--- pages/api/blog/get-all-entries.ts
+++ pages/api/blog/get-all-entries.ts
@@ -8,7 +8,7 @@ export default async function handle(req: NextApiRequest, res: NextApiResponse) 
 	res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
 	res.setHeader(
 		'Access-Control-Allow-Headers',
-		'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+		'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
 	);
 	try {
 		const result = await prisma.blog.findMany({
