cb9969293857dfe711acd6dfe8746b65a11cd517
alur2191 <dandavisjs@protonmail.com>
2023-04-01 17:21:40 -0700

    Changed CORS settings

--- next.config.js
+++ next.config.js
@@ -6,6 +6,14 @@ const nextConfig = {
 		formats: ['image/avif', 'image/webp'],
 		unoptimized: true,
 	},
+	// matching all API routes
+	source: '/api/:path*',
+	headers: [
+		{ key: 'Access-Control-Allow-Credentials', value: 'true' },
+		{ key: 'Access-Control-Allow-Origin', value: '*' },
+		{ key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
+		{ key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version' },
+	],
 };
 
 module.exports = nextConfig;
