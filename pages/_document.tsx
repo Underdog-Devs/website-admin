e4dfed368fed639abd1d4cff5584d0f0f13cfeff
alur2191 <dandavisjs@protonmail.com>
2023-05-19 18:23:37 -0700

    Added page title

---
+++ pages/_document.tsx
@@ -0,0 +1,56 @@
+import Document, {
+	DocumentContext,
+	Html,
+	Head,
+	Main,
+	NextScript,
+} from 'next/document';
+import SiteHead from '../components/siteHead';
+
+class MyDocument extends Document {
+	static async getInitialProps(ctx: DocumentContext) {
+		const initialProps = await Document.getInitialProps(ctx);
+
+		return initialProps;
+	}
+
+	render() {
+		return (
+			<Html lang="en-US">
+				<Head>
+					<SiteHead
+						title="Underdog Devs - Admin panel"
+						description="We are a group of software engineers supporting formerly incarcerated and disadvantaged aspiring developers"
+					/>
+					<link
+						rel="preconnect"
+						href="https://fonts.gstatic.com"
+						crossOrigin="true"
+					/>
+					<link rel="preconnect" href="https://fonts.googleapis.com" />
+					<link rel="preconnect" href="https://fonts.gstatic.com" />
+					<link
+						href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@400;700&family=Open+Sans:wght@400;600;700&family=Quicksand:wght@400;700&family=Raleway:wght@400;700&family=Roboto:ital,wght@0,400;1,700&family=Source+Sans+Pro:wght@400;700&display=swap"
+						rel="stylesheet"
+					/>
+					<link rel="preconnect" href="https://fonts.googleapis.com" />
+					<link
+						rel="preconnect"
+						href="https://fonts.gstatic.com"
+						crossOrigin="true"
+					/>
+					<link
+						href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@400;500;600;700&display=swap"
+						rel="stylesheet"
+					/>
+				</Head>
+				<body>
+					<Main />
+					<NextScript />
+				</body>
+			</Html>
+		);
+	}
+}
+
+export default MyDocument;
